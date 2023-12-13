c9fd4f87804ae8f0fcc8d7da4ba87d57d033bb5a
Sridhar <sridharmoturu@gmail.com>
2023-12-13 20:28:14 +0530

    check deploy

--- Controllers/checkOut.js
+++ Controllers/checkOut.js
@@ -7,13 +7,22 @@ import Razorpay from "razorpay";
 import NotFoundError from "../errors/not-found.js";
 import BadRequestError from "../errors/bad-request.js";
 
-
 // checkout route       route : /api/create-checkout
-const checkOut = async()=> {
-    
-}
+const checkOut = async (req, res) => {
+  const data = req.body;
+  console.log("request came here");
+  const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID,
+    key_secret: process.env.RAZORPAY_KEY_SECRET,
+  });
+console.log(data)
+  const order = await razorpay.orders.create(data);
+  console.log(order)
+  if (!order)
+    return res.status(StatusCodes.BAD_REQUEST).json("Order creation failed");
+  res.status(StatusCodes.OK).json(order);
+};
 
 router.post("/", checkOut);
 
 export default router;
-
