fc2fac9b4e696b2b26e5ca3010224f9b0717610e
Sridhar <sridharmoturu@gmail.com>
2023-12-28 22:01:43 +0530

    token time changed

--- Model/userModel.js
+++ Model/userModel.js
@@ -54,7 +54,7 @@ UserSchema.methods.createToken = async function () {
   const token = await jwt.sign(
     { id: this._id, isAdmin: this.isAdmin },
     process.env.JWT_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: '1d' }
   );
   return token;
 };
